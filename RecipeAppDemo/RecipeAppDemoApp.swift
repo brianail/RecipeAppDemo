62c3892ac8da0a168c4cfd09569ec4889d79bca3
brianail <80833852+brianail@users.noreply.github.com>
2022-01-20 22:38:55 -0300

    Initial Commit

---
+++ RecipeAppDemo/RecipeAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppDemoApp.swift
+//  RecipeAppDemo
+//
+//  Created by Brian Ail on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
